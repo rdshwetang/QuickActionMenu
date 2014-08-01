52415a5c989efc6675cb5e4d0864b07ae621d1fd
rdshwetang <shwetang.bms@gmail.com>
2014-08-01 15:03:12 -0300

    Uploading project QuickActionMenu

---
+++ gen/com/org/QuickActionActivity/BuildConfig.java
@@ -0,0 +1,10 @@
+/* 
+ * @author Shwetang R.D 
+ * Visual Sistemas Electronicos Ltda.
+ * Belo Horizonte-Brazil -2014
+ */
+package com.org.QuickActionActivity;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/visuals/QuickActionActivity/ActionItem.java
@@ -0,0 +1,45 @@
+/* 
+ * @author Shwetang R.D 
+ * Visual Sistemas Electronicos Ltda.
+ * Belo Horizonte-Brazil -2014
+ */
+package com.visuals.QuickActionActivity;
+
+import android.graphics.drawable.Drawable;
+import android.graphics.Bitmap;
+
+public class ActionItem {
+	private Drawable icon;
+	private Bitmap thumb;
+	private String title;
+	private boolean selected;
+	
+	public ActionItem() {}
+	public ActionItem(Drawable icon) {
+		this.icon = icon;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getTitle() {
+		return this.title;
+	}
+	public void setIcon(Drawable icon) {
+		this.icon = icon;
+	}
+	public Drawable getIcon() {
+		return this.icon;
+	}
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+	public boolean isSelected() {
+		return this.selected;
+	}
+	public void setThumb(Bitmap thumb) {
+		this.thumb = thumb;
+	}
+	public Bitmap getThumb() {
+		return this.thumb;
+	}
+}
